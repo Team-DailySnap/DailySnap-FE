6a5c2883b6f886835809edf80a25d4c76d33ce56
Nam Yooseong <nys8765@naver.com>
2025-07-09 02:21:54 +0900

    chore: react-native-svg, react-native-svg-transformer 추가

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,19 @@
 const { getDefaultConfig } = require("expo/metro-config");
 
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
-config.resolver.sourceExts.push("css");
-config.resolver.assetExts.push("png", "jpg", "jpeg", "gif", "svg");
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname);
 
-module.exports = config;
+  const { transformer, resolver } = config;
+
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer/expo"),
+  };
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter(ext => ext !== "svg"),
+    sourceExts: [...resolver.sourceExts, "svg"],
+  };
+
+  return config;
+})();
