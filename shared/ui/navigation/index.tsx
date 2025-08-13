66a0cb79d33d574d3842ebcf15deddbc5c7a5644
Nam Yooseong <nys8765@naver.com>
2025-08-13 15:38:47 +0900

    style: 네비게이션 디자인 수정

--- shared/ui/navigation/index.tsx
+++ shared/ui/navigation/index.tsx
@@ -4,13 +4,16 @@ export const DefaultNavigationOptions = {
   tabBarActiveTintColor: basicColors.green100,
   tabBarInactiveTintColor: basicColors.green50,
   tabBarIconStyle: {
+    marginTop: 4,
     marginBottom: 4,
     width: 32,
     height: 32,
   },
   tabBarStyle: {
+    position: "absolute",
     paddingHorizontal: 34,
-    backgroundColor: basicColors.beige,
+    borderRadius: 24, // 모서리 둥글기
+    height: 100, // 탭바 높이
   },
   tabBarLabelStyle: {
     fontSize: 12,
